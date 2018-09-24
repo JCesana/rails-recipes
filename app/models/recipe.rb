38e7b6bb6f9526be3b3f31fc2f3a30ee0916b3bc
JCesana <joshuacesana@gmail.com>
2018-09-24 00:07:12 -0700

    add image methods to resize

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
-  has_one_attached :image 
+  has_one_attached :image
+
+  def medium
+    return self.image.variant(resize: '400x400')
+  end
+
+  def thumbnail
+    return self.image.variant(resize: '200x200')
+  end
 end
