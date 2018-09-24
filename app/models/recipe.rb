e07f1a48eb3cc5b77dd3c029fca127a9f8c78d93
JCesana <joshuacesana@gmail.com>
2018-09-24 14:43:51 -0700

    refactor accepts_nested_attributes_for

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,14 +3,10 @@ class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :directions
 
-  accepts_nested_attributes_for :ingredients,
-                                reject_if: proc { |attributes| attributes['name'].blank? },
-                                allow_destroy: true
-  accepts_nested_attributes_for :directions,
-                                reject_if: proc { |attributes| attributes['step'].blank? },
-                                allow_destroy: true
+  accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :directions, :reject_if => :all_blank, :allow_destroy => true
 
-  validates :title, :description, :image, presence: true 
+  validates :title, :description, :image, presence: true
 
   has_one_attached :image
 
