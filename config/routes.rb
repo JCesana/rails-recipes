0301949353f2c17823f8b20950666ed434a9cce6
JCesana <joshuacesana@gmail.com>
2018-09-25 20:20:53 -0700

    add nested comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'comments/create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :recipes
+  resources :recipes do
+    post 'comments', to: 'comments#create'
+  end
 
   root 'recipes#index'
 end
