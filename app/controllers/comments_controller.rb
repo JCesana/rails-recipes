9fafdd39a5c87695ac20676145b8899f6cf25f5d
JCesana <joshuacesana@gmail.com>
2018-09-25 20:17:32 -0700

    build create, private methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :set_recipe
+  before_action :authenticate_user!
+
   def create
+    @comment = @recipe.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :recipe_id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
