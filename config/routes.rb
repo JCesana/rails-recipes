1a4e92ec08a545087211f5506148b3d97925c792
JCesana <joshuacesana@gmail.com>
2018-10-01 21:50:20 -0700

    change routes for comments, add routes for omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :recipes do
-    post 'comments', to: 'comments#create'
+    resources :comments, only: [:show, :create]
+    # post 'commments', to: 'comments#create'
   end
 
   root 'recipes#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
