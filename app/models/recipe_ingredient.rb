08aec326fd0bc491a70e69dfa123746e14598756
JCesana <joshuacesana@gmail.com>
2018-10-04 01:26:13 -0700

    delete unneeded methods, refactor recipe_line_detail method

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -8,7 +8,7 @@ class RecipeIngredient < ApplicationRecord
   validates_uniqueness_of :recipe_id, :scope => :ingredient_id
 
   def recipe_line_detail
-    "#{pluralize(clean_number(ri.ingredient_quantity), ri.ingredient_unit)} #{ri.ingredient_name}"
+    "#{clean_number(self.quantity.to_f)} #{self.unit.pluralize(self.quantity.to_f)} #{self.ingredient.name}"
   end
 
   # only shows decimal if needed, otherwise integer
@@ -16,16 +16,4 @@ class RecipeIngredient < ApplicationRecord
     "%.3g" % ("%.2f" % num)
   end
 
-  def ingredient_name
-    ingredient = Ingredient.find_by(id: self.ingredient_id)
-    ingredient.name
-  end
-
-  def ingredient_quantity
-    self.quantity.to_f
-  end
-
-  def ingredient_unit
-    self.unit
-  end
 end
