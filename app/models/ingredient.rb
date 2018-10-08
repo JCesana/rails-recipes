e00d824b0469ea3915444f54290cfd1cf7f43eda
JCesana <joshuacesana@gmail.com>
2018-10-08 16:18:41 -0700

    delete accepts_nested_attributes_for

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients, inverse_of: :ingredient
   has_many :recipes, through: :recipe_ingredients
-
   validates :name, uniqueness: true
-
-  accepts_nested_attributes_for :recipe_ingredients, :reject_if => :all_blank, :allow_destroy => true
 end
