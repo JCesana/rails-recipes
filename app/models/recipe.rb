5ff764c6330c428ea1109ead15e83fcf68adee92
JCesana <joshuacesana@gmail.com>
2018-10-09 13:46:48 -0700

    Fix typo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,7 +7,7 @@ class Recipe < ApplicationRecord
   has_one_attached :image
 
   accepts_nested_attributes_for :directions, :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :recipe_ingredients, :allow_destroy => true
+  accepts_nested_attributes_for :recipe_ingredients, :reject_if => :all_blank, :allow_destroy => true
 
   validates :title, :description, presence: true
 end
