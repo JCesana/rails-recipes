3dc82859d46ed0d2f81efea62c9f418cb1819ea1
JCesana <joshuacesana@gmail.com>
2018-10-09 19:26:45 -0700

    Add routes for custom scopes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  root 'recipes#index'
+
+  get 'recipes/most_active',                to: 'recipes#active'
+  get 'recipes/most_ingredients',           to: 'recipes#most_ingredients'
+  get 'recipes/least_ingredients',          to: 'recipes#least_ingredients'
+  get 'recipes/by_ingredient/:ingredient',  to: 'recipes#by_ingredient'
+
   resources :recipes do
     resources :comments, only: [:show, :create]
-    # post 'commments', to: 'comments#create'
   end
-
-  root 'recipes#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
