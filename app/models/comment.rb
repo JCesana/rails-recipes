24430852542c6edbdc9be5efec1373e99eec9aaa
JCesana <joshuacesana@gmail.com>
2018-10-09 19:25:32 -0700

    Add counter_cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :recipe
-  validates :body, presence: true, allow_blank: false 
+  belongs_to :recipe, counter_cache: true 
+  validates :body, presence: true, allow_blank: false
 end
