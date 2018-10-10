edbf06e858001958b5bc6e19091b3bd88dc6bbda
JCesana <joshuacesana@gmail.com>
2018-10-09 19:25:53 -0700

    Add counter_cache

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,6 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
-  belongs_to :recipe
+  belongs_to :recipe, counter_cache: true
 
   accepts_nested_attributes_for :ingredient, :reject_if => :all_blank, :allow_destroy => true
 
