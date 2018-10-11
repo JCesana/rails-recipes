37604ff349fc87b46517a128395ca168dc8ea08b
JCesana <joshuacesana@gmail.com>
2018-10-10 20:47:23 -0700

    Erase ingredient_attributes, add custom setter/getter

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -4,13 +4,13 @@ class RecipeIngredient < ApplicationRecord
 
   accepts_nested_attributes_for :ingredient, :reject_if => :all_blank, :allow_destroy => true
 
-  def ingredient_attributes=(ingredient_attributes)
-    ingredient_attributes.values.each do |attribute|
-      if attribute != ""
-        ingredient = Ingredient.find_or_create_by(name: attribute)
-        self.ingredient = ingredient
-      end
-    end
+  def ingredient_name
+    self.ingredient.name if !self.id.nil?
+  end
+
+  def ingredient_name=(name)
+    @ingredient = Ingredient.find_or_create_by(name: name)
+    self.ingredient = @ingredient
   end
 
   def recipe_line_detail
