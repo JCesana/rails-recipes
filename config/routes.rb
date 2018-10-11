c4a32552af8fe0077004817136ffc8feeba01c3b
JCesana <joshuacesana@gmail.com>
2018-10-11 12:46:34 -0700

    Refactor URLs

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
 
   root 'recipes#index'
 
-  get 'recipes/most_active',                  to: 'recipes#active'
-  get 'recipes/most_ingredients',             to: 'recipes#most_ingredients'
-  get 'recipes/least_ingredients',            to: 'recipes#least_ingredients'
+  get 'recipes/most_active',       to: 'recipes#active'
+  get 'recipes/hardest',  to: 'recipes#most_ingredients'
+  get 'recipes/easiest', to: 'recipes#least_ingredients'
 
   resources :recipes do
     resources :comments, only: [:show, :create]
+  end
+
+  resources :ingredients do
+    resources :recipes, only: [:index, :show]
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
