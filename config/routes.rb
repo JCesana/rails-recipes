bdb62562f985672b3ee2c2e1c9b72d7b429e9368
JCesana <joshuacesana@gmail.com>
2018-10-10 20:49:51 -0700

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   root 'recipes#index'
 
-  get 'recipes/most_active',                to: 'recipes#active'
-  get 'recipes/most_ingredients',           to: 'recipes#most_ingredients'
-  get 'recipes/least_ingredients',          to: 'recipes#least_ingredients'
-  get 'recipes/by_ingredient/:ingredient',  to: 'recipes#by_ingredient'
+  get 'recipes/most_active',                  to: 'recipes#active'
+  get 'recipes/most_ingredients',             to: 'recipes#most_ingredients'
+  get 'recipes/least_ingredients',            to: 'recipes#least_ingredients'
 
   resources :recipes do
     resources :comments, only: [:show, :create]
