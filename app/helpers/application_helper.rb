56c7eff0b0f6d14ccc675cd82529425e1dcdb169
JCesana <joshuacesana@gmail.com>
2018-10-13 14:29:52 -0700

    Add helper method to clean up alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def clean_alert(alert)
+    if alert.is_a?(Array)
+      alert = alert.join(". ")
+      alert = alert + "." if alert[-1] != "."
+    else
+      alert = alert + "." if alert[-1] != "."
+    end
+
+    alert
+  end
+    
 end
