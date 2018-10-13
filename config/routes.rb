29d6f7bc221358b8f15d706b8f17fd22f60d25eb
JCesana <joshuacesana@gmail.com>
2018-10-13 14:33:53 -0700

    Refactor url text & nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
 
   root 'recipes#index'
 
-  get 'recipes/most_active',       to: 'recipes#active'
-  get 'recipes/hardest',  to: 'recipes#hardest'
-  get 'recipes/easiest', to: 'recipes#easiest'
+  get 'recipes/most_active',  to: 'recipes#active'
+  get 'recipes/hardest',      to: 'recipes#hardest'
+  get 'recipes/easiest',      to: 'recipes#easiest'
+
+  get 'by_ingredient',       to: 'ingredients#show'
 
   resources :recipes do
     resources :comments, only: [:show, :create]
   end
 
-  resources :ingredients do
-    resources :recipes, only: [:index, :show]
-  end
+  resources :ingredients, only: [:index, :show]
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
