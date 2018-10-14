c321871dbc0b5cd765bddb4f4324c36ba2671b81
JCesana <joshuacesana@gmail.com>
2018-10-14 00:07:34 -0700

    Add scope for top_contributors

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,19 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :comments, dependent: :destroy
 
+  scope :top_contributors, -> { User.joins(:recipes).group("user_id").order("count(recipes.id) DESC").limit(25) }
+
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails,
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
   end
-end
 
 
 end
