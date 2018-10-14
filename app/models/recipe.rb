6604360aa13e83b98793f166a7dbf877acf41888
JCesana <joshuacesana@gmail.com>
2018-10-14 16:30:51 -0700

    Add dependent destroys to fix destroy action

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_ingredients, inverse_of: :recipe
+  has_many :recipe_ingredients, inverse_of: :recipe, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
   has_many :directions, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
 
   accepts_nested_attributes_for :directions, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :recipe_ingredients, :reject_if => :all_blank, :allow_destroy => true
