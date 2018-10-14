822afb09445928a9145ce456a4ff079e45c4cfc1
JCesana <joshuacesana@gmail.com>
2018-10-14 00:10:10 -0700

    Add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/top_contributors'
   root 'recipes#index'
 
-  get 'recipes/most_active',  to: 'recipes#active'
-  get 'recipes/hardest',      to: 'recipes#hardest'
-  get 'recipes/easiest',      to: 'recipes#easiest'
+  get 'recipes/most_active',    to: 'recipes#active'
+  get 'recipes/hardest',        to: 'recipes#hardest'
+  get 'recipes/easiest',        to: 'recipes#easiest'
 
-  get 'by_ingredient',       to: 'ingredients#show'
+  get 'users/top_contributors',  to: 'users#top_contributors', as: 'top_contributors'
+
+  get 'by_ingredient',          to: 'ingredients#show'
+
+  resources :users, only: [:index, :show] do
+    resources :recipes, only: [:index]
+  end
 
   resources :recipes do
-    resources :comments, only: [:index, :show, :create]
+    resources :comments, only: [:index, :create]
   end
 
   resources :ingredients, only: [:index, :show]
