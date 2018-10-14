959112375f88e5d28335299c4ad92a9b4f064547
JCesana <joshuacesana@gmail.com>
2018-10-14 16:05:53 -0700

    Add columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_012809) do
+ActiveRecord::Schema.define(version: 2018_10_14_210329) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -87,10 +87,10 @@ ActiveRecord::Schema.define(version: 2018_10_10_012809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "image"
+    t.string "name"
     t.string "provider"
     t.string "uid"
-    t.string "name"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
