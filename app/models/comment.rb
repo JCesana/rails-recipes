b77fbe6f17039c69ee71f9c9b61b2686edbdf282
JCesana <joshuacesana@gmail.com>
2018-11-25 12:58:58 -0800

    Add user_name method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :recipe, counter_cache: true 
+  belongs_to :recipe, counter_cache: true
   validates :body, presence: true, allow_blank: false
+
+  def user_name
+    self.user.name 
+  end
 end
