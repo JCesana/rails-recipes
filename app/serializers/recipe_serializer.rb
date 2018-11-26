c7f077c5fcfb694b25524cd1bc3bf1ae2bd8ad68
JCesana <joshuacesana@gmail.com>
2018-11-25 17:06:50 -0800

    Add comments, ingredients, directions to attributes

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,8 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :comments, :ingredients, :directions 
 
   has_many :comments
-  belongs_to :user 
+  belongs_to :user
+  has_many :ingredients
+  has_many :directions
 end
