f7fccb555bd0c9eb2f7cc1c532909601368ca9b0
JCesana <joshuacesana@gmail.com>
2018-11-26 18:40:02 -0800

    Add recipe_ingredients to attributes

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,8 +1,9 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :comments, :ingredients, :directions 
+  attributes :id, :title, :description, :comments, :directions, :ingredients, :recipe_ingredients
 
   has_many :comments
   belongs_to :user
   has_many :ingredients
+  has_many :recipe_ingredients
   has_many :directions
 end
