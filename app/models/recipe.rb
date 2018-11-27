633b6c817144ff2223b40195340847a32e050f96
JCesana <joshuacesana@gmail.com>
2018-11-26 20:08:38 -0800

    Add image_url method for JS next/previous feature

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -34,4 +34,8 @@ class Recipe < ApplicationRecord
       self.class.order("title DESC").first
     end
   end
+
+  def image_url
+    Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true)
+  end
 end
