7786300759262ce9a7477dcc7fe297db3f58c0ff
JCesana <joshuacesana@gmail.com>
2018-11-26 20:09:16 -0800

    Add image_url method to serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,9 +1,9 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :comments, :directions, :ingredients, :recipe_ingredients
+  attributes :id, :title, :description, :comments, :directions, :ingredients, :recipe_ingredients, :image_url
 
   has_many :comments
   belongs_to :user
   has_many :ingredients
-  has_many :recipe_ingredients
   has_many :directions
+  has_many :recipe_ingredients
 end
