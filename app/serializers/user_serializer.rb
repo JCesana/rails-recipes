7e89a9538423336a8fef52e1fb18390dbecd1aba
JCesana <joshuacesana@gmail.com>
2018-11-27 19:35:26 -0800

    Add current_user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :current_user
 
   has_many :comments
-  has_many :recipes 
+  has_many :recipes
 end
